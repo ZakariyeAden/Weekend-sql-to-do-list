fed3358e542284a3d91f26b32b97fa1b1e15c5c8
ZakariyeAden <zakariye5417@gmail.com>
2023-06-23 19:24:34 -0500

    worked on JQ and setting up routes

---
+++ Server/Public/Client/client.js
@@ -0,0 +1,8 @@
+console.log('In JS file');
+
+
+$(document).ready(readyNow);
+
+function readyNow(){
+  console.log('JQ');
+}
--- Server/Public/Routes/todo.router.js
+++ Server/Public/Routes/todo.router.js
@@ -4,9 +4,22 @@ const pool = require('../../Modules/pool');
 const router = express.Router();
 
 
-
+// GET
 router.get('/', (req,res) => {
+  // Get all of the data from table
+  let getQueryTodo = 'SELECT * FROM todo';
 
+  pool.query(getQueryTodo)
+  // Wanna get the results from the database
+  .then((result) => {
+    // See the results in the Log
+    console.log('Results:', result)
+    // Send the Result Rows
+    res.send(result.rows);
+    // Catch any Errors
+  }).catch((error) => {
+    console.log(`Error in ${getQueryTodo} GET:`, error);
+  })
 })
 
 
