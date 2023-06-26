fd4fe7f043ab941c2523bc20a32cab6f2021ee10
ZakariyeAden <zakariye5417@gmail.com>
2023-06-26 16:10:42 -0500

    fix

--- Server/Modules/pool.js
+++ Server/Modules/pool.js
@@ -1,14 +1,32 @@
 // Get Postgres
 const pg = require('pg');
 
-const pool = new pg.Pool({
-  // The database to connect
-  database: 'weekend-to-do-app',
-  // Our Localhost
-  host: 'localhost',
-  // Port to listen in Pg
-  port:5432
-})
+
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'weekend-to-do-app'
+  })
+}
+
+// const pool = new pg.Pool({
+//   // The database to connect
+//   database: 'weekend-to-do-app',
+//   // Our Localhost
+//   host: 'localhost',
+//   // Port to listen in Pg
+//   port:5432
+// })
 
 // Export Pool
 module.exports = pool
